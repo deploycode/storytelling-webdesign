b46ee78464d0dbca9ee988f8ccaef4df898ded01
DeployCode <social@deploycode.co>
2017-11-21 19:31:51 -0500

    Update controller.js

--- js/controller.js
+++ js/controller.js
@@ -1,50 +1,12 @@
 angular.module("myApp", [])
 
-.controller("myController", function($scope){
-  $scope.newComentario={};
-  $scope.points = [
-    {
-      name: "Bogotá",
-      location: "s1"
-    },
-    {
-      name: "Lechonería mcgordo",
-      location: "s2"
-    },
-    {
-      name: "Restaurante en Quetame",
-      location: "s5"
-    },
-    {
-      name: "Tequendema Vacalay",
-      location: "s10"
-    }
-  ]
-  $scope.vars = [
-    {
-      name: "clima",
-      value: "soleado",
-      icon: "fa-sun-o"
-    },
-    {
-      name: "tiempo estimado de viaje",
-      value: "02:00:00",
-      icon: "fa-clock-o"
-    },
-    {
-      name: "distancia estimada",
-      value: "2km",
-      icon: "fa-minus"
-    },
-    {
-      name: "tráfico",
-      value: "bueno",
-      icon: "fa-exclamation-triangle"
-    },
-
-  ];
-  $scope.addComentario = function(){
-    $scope.comentarios.push($scope.newComentario);
-    $scope.newComentario={};
-  }
+.controller("myController", function($scope , $http){
+  $scope.points={};
+  $http.get("http://18.221.159.112:6700/getdataplaces")
+    .success(function(data){
+      $scope.points= data;
+    })
+    .error(function(err){
+      console.log(err);
+    });
 });
